5298618e292f8fb1db26d707a2bebd85ebe6c259
Michael L. Collard <collard@uakron.edu>
2023-01-19 11:37:26 -0500

    Separate calculation of average and heaviest

--- rainfall.cpp
+++ rainfall.cpp
@@ -22,13 +22,17 @@ int main() {
         return 1;
     }
 
-    // calculate average and heaviest rainfall
-    auto total = rainfall[0];
+    // calculate heaviest rainfall
     auto heaviest = rainfall[0];
     for (std::vector<double>::size_type i = 1; i < rainfall.size(); ++i) {
-        total += rainfall[i];
         if (rainfall[i] > heaviest)
             heaviest = rainfall[i];
+    }
+
+    // calculate average rainfall
+    auto total = rainfall[0];
+    for (std::vector<double>::size_type i = 1; i < rainfall.size(); ++i) {
+        total += rainfall[i];
     }
 
     // output the rainfall report
