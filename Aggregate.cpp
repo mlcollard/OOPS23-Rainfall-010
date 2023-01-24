7d2d2f2ca07106d24c3ff1021a083b60c2937ce3
Michael L. Collard <collard@uakron.edu>
2023-01-24 12:03:10 -0500

    Add definition of max() to separate Aggregate and call in main program

---
+++ Aggregate.cpp
@@ -0,0 +1,19 @@
+/*
+    Aggregate.cpp
+
+    Implementation file for aggregate functions
+*/
+
+#include "Aggregate.hpp"
+
+// maximum of the data
+double max(const std::vector<double>& data) {
+
+    auto maximum = data[0];
+    for (const auto value: data) {
+        if (value > maximum)
+            maximum = value;
+    }
+
+    return maximum;
+}
--- Aggregate.hpp
+++ Aggregate.hpp
@@ -7,6 +7,8 @@
 #ifndef INCLUDED_AGGREGATE_HPP
 #define INCLUDED_AGGREGATE_HPP
 
+#include <vector>
+
 // maximum of the data
 double max(const std::vector<double>& data);
 
