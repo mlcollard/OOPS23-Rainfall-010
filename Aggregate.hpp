5ae30fd720509dd5aa804942f30172fd98b79416
Michael L. Collard <collard@uakron.edu>
2023-01-24 11:59:04 -0500

    Add declaration of max() to separate Aggregate file

---
+++ Aggregate.hpp
@@ -0,0 +1,13 @@
+/*
+    Aggregate.hpp
+
+    Include file for aggregate functions
+*/
+
+#ifndef INCLUDED_AGGREGATE_HPP
+#define INCLUDED_AGGREGATE_HPP
+
+// maximum of the data
+double max(const std::vector<double>& data);
+
+#endif
